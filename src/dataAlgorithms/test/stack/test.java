c0c1aa52ec0ef0440c958f70b08b0d5bdeeb2274
dxzmpk <1635338905@qq.com>
2019-03-21 18:11:47 +0800

    #dev2 add more message

--- src/dataAlgorithms/test/stack/test.java
+++ src/dataAlgorithms/test/stack/test.java
@@ -3,5 +3,6 @@ package dataAlgorithms.test.stack;
 public class test {
     public static void main(String[] args){
         System.out.println("dev");
+        System.out.println("dev more things");
     }
 }
